4c6ca995b58872e9c8c21e89d9aee2d6874746a9
Archie <archie.calvert@outlook.com>
2024-12-29 15:57:45 +0000

    Installed JoyUI, and fixed an issue with the header being position:absolute

--- tellmewhen/src/components/Header.jsx
+++ tellmewhen/src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header()
 {
     return(
         // containter div
-        <div className = "w-full absolute top-[0px] h-[100px] bg-[#0A5397]">
+        <div className = "w-full top-[0px] h-[100px] bg-[#0A5397]">
             {/* div used for centering text vertically and horizontally */}
             <div className = "w-full h-full flex items-center justify-center">
                 <h1 className = "text-[50px] text-white font-bebas-neue">Tell Me When</h1>
