0e85889c12acf60be16e10bb0dcbe1c4ac15d3d7
Archie <archie.calvert@outlook.com>
2024-12-30 22:17:14 +0000

    Made the table controls sticky on the dashboard

--- tellmewhen/src/app/dashboard/page.jsx
+++ tellmewhen/src/app/dashboard/page.jsx
@@ -11,24 +11,26 @@ import {Button, Tab, Tabs, TabList, tabClasses} from "@mui/joy"
 import {Add} from "@mui/icons-material"
 import Pagination from "@/components/Pagination";
 
-
 function Page()
 {
     let colours = {
         "header": "#0A5397", //Original colour of the header
         "primary": "rgba(11,107,203,1)",
         "primary-text-colour": "rgba(255,255,255,1)",
-        "secondary": "rgba(227,225,225,1)",
+        "secondary": "#e3e1e1",
         "secondary-text-colour": "rgba(103,107,111,1)",
-        "tertiary": "rgba(240,244,248,1)", //used when highlighting a secondary colour item (switcher)
+        "tertiary": "#e9e9e9", //used when highlighting a secondary colour item (switcher)
         "tertiary-text-colour": "rgba(11,13,14,1)",
     };
     let coloursTailwind = {
         "primary": `bg-[rgba(11,107,203,1)]`,
         "primary-text-colour": `text-[rgba(255,255,255,1)]`,
+        "secondary": "bg-[#e3e1e1]",
+        "tertiary": "bg-[rgba(233,233,233,1)]", //used when highlighting a secondary colour item (switcher)
+        "tertiary-semi-transparent": "bg-[rgba(233,233,233,0.9)]",
         //Table Colours
         "rows-colour1": `bg-[rgb(210,214,218)]`,
-        "rows-colour2": `bg-[rgb(230, 234, 243)]`,
+        "rows-colour2": `bg-[rgb(230,234,243)]`,
         "rows-text-colour": `text-[${colours["rows-text-colour"]}]`,
 
     }
@@ -40,12 +42,15 @@ function Page()
     const [DisplayedTableData, SetDisplayedTableData] = useState([[]]);
 
     const CurrentTableHeaders = ["Job ID", "User ID", "Description", "Due"];
+    const CurrentTableWidths = ["w-[10%]", "w-[10%]", "w-[70%]", "w-[10%]"];
     const HistoryTableHeaders = ["Job ID", "User ID", "Description",];
+    const HistoryTableWidths = ["w-[10%]", "w-[10%]", "w-[80%]"];
     //Used to determine which is the currently selected data (Current=0 or History=1)
     const [CurrentIndex, SetIndex] = useState(0);
 
     const [PageNumber, SetPageNumber] = useState(1);
 
+    document.title = "Dashboard | Tell Me When";
     useEffect(() => {
         //This code will run when the value of "CurrentIndex" changes
 
@@ -116,8 +121,8 @@ function Page()
         
         <div className = {"page-content w-[80%] m-auto"}>
             { /* Span element is for the controls above the table (Switcher and Button) to keep them inline*/ }
-            <span className = "w-full flex mt-[20px] justify-between items-center h-[60px] overflow-hidden">
-                <Tabs className = "w-[200px] outline outline-[0px] outline-[#B8B8B8] rounded-[10px]"
+            <span className = {`sticky top-[15px] ${coloursTailwind["tertiary"]} border-[1px] mb-[10px] border-[#A0A0A0] w-[98%] m-auto rounded-lg shadow-lg flex mt-[20px] justify-between items-center h-[60px]`}>
+                <Tabs className = "w-[200px] outline ml-[6px] outline-[1px] outline-[#B8B8B8] rounded-[10px]"
                       onChange={(event, index) => {SetIndex(index);}}>
                     <TabList
                             disableUnderline
@@ -149,7 +154,7 @@ function Page()
                             <Tab disableIndicator>History</Tab>
                     </TabList>
                 </Tabs>
-                <span>
+                <span className="mr-[10px]">
                     <Button endDecorator={<Add/>}
                             sx={{
                                 fontWeight: "650",
@@ -167,9 +172,9 @@ function Page()
                     <tr className = {`${coloursTailwind["primary"]} ${coloursTailwind["primary-text-colour"]} h-[40px]`}>
                         {/* Ternary operator for an if statement to determine which table headers are to be displayed */}
                         {CurrentIndex == 0 ? CurrentTableHeaders.map((item, index) => {
-                                return <th key={index} className={`px-[10px] ${index!=0?`border-l-[2px] border-[rgba(0,0,0,0.2)]`:{}}`}>{item}</th>
+                                return <th key={index} className={`px-[10px] ${index!=0?`border-l-[2px] border-[rgba(0,0,0,0.2)]`:{}} ${CurrentTableWidths[index]}`}>{item}</th>
                             }) : HistoryTableHeaders.map((item, index) => {
-                                    return <th key={index} className="px-[10px] border-l-[2px] border-[rgba(0,0,0,0.2)]">{item}</th>
+                                    return <th key={index} className={`px-[10px] ${index!=0?`border-l-[2px] border-[rgba(0,0,0,0.2)]`:{}} ${HistoryTableWidths[index]}`}>{item}</th>
                             })
                         }
                     </tr>
@@ -187,14 +192,15 @@ function Page()
                 </tbody>
             </table>
             </div>
-            <Pagination className = "fixed w-[80%] bottom-[20px] justify-self-center flex justify-center"
+            <Pagination className = "sticky w-[100%] mt-[20px] bottom-[15px] justify-self-center flex justify-center"
                         buttonColour={colours["primary"]}
                         fontColourButtons={colours["primary-text-colour"]}
                         SetPageNumber={SetPageNumber}
                         PageNumber={PageNumber}
+                        bgColour={colours["tertiary"]}
                         border="true"/>
             
-            <div className="bottom-margin mb-[80px]" />
+            <div className="bottom-margin mb-[30px]" />
         </div>
     );
 }
