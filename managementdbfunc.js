dbd2c1669b17ec8bc6e5d7d1c2073a2fe77149e1
Hufsa Haq <hufsahaq@gmail.com>
2025-01-28 17:50:42 +0000

    Conn database and new funct to execute

--- managementdbfunc.js
+++ managementdbfunc.js
@@ -8,3 +8,32 @@
 // chnage privilege levels
 
 
+import mysql from 'mysql';
+
+const db = mysql.createConnection({
+  host: 'dbhost.cs.man.ac.uk',
+  user: 'z26101hh',
+  password: 'UTZxLV/au62nauNC7XxBhNsvh5Wm7CcShrtKz4bwj24',
+  database: 'tellmewhen',
+});
+
+// Connect to the database
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.message);
+  } else {
+    console.log('Connected to the MySQL database.');
+  }
+});
+
+// Helper function to execute queries
+const executeQuery = (sql, params = []) =>
+  new Promise((resolve, reject) => {
+    db.query(sql, params, (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  });
