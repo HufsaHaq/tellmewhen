8520fddc164cb3101d10639a2052bca55c5ceb65
Hufsa Haq <hufsahaq@gmail.com>
2025-01-28 18:13:14 +0000

    delete entire business account

--- managementdbfunc.js
+++ managementdbfunc.js
@@ -40,29 +40,90 @@ const executeQuery = (sql, params = []) =>
 
 // Add worker/manager/admin
 export const addUser = async (username, hashedPassword, businessId, privilegeLevel) => {
-    const sql = `
-      INSERT INTO WORKER_TABLE (Username, Hashed_Password, Business_ID, Privilege_level)
-      VALUES (?, ?, ?, ?);
-    `;
-    return executeQuery(sql, [username, hashedPassword, businessId, privilegeLevel]);
-  };
-  
+  const sql = `
+    INSERT INTO WORKER_TABLE (Username, Hashed_Password, Business_ID, Privilege_level)
+    VALUES (?, ?, ?, ?);
+  `;
+  return executeQuery(sql, [username, hashedPassword, businessId, privilegeLevel]);
+};
+
 // Delete worker/manager/admin
 export const deleteUser = async (workerId) => {
-    const sql = `
-      DELETE FROM WORKER_TABLE WHERE Worker_ID = ?;
-    `;
-    return executeQuery(sql, [workerId]);
-  };
+  const sql = `
+    DELETE FROM WORKER_TABLE WHERE Worker_ID = ?;
+  `;
+  return executeQuery(sql, [workerId]);
+};
+
+// Edit worker/manager/admin login details
+export const editUserLogin = async (workerId, newUsername, newPassword) => {
+  const sql = `
+    UPDATE WORKER_TABLE
+    SET Hashed_Password = ?
+    WHERE Worker_ID = ? AND Username = ?;
+  `;
+  return executeQuery(sql, [newPassword, workerId, newUsername]);
+};
 
 // Delete business account and all associated accounts
 export const deleteBusiness = async (businessId) => {
-    const sqlWorkers = `DELETE FROM WORKER_TABLE WHERE Business_ID = ?;`;
-    const sqlBusiness = `DELETE FROM BUSINESS_TABLE WHERE Business_ID = ?;`;
+  const sqlWorkers = `DELETE FROM WORKER_TABLE WHERE Business_ID = ?;`;
+  const sqlBusiness = `DELETE FROM BUSINESS_TABLE WHERE Business_ID = ?;`;
+
+  // need to delete workers then the business
+  await executeQuery(sqlWorkers, [businessId]);
+  return executeQuery(sqlBusiness, [businessId]);
+};
+
+// Get the number of open jobs
+export const countOpenJobs = async () => {
+    // SQL query to count the number of open jobs
+    const sql = `SELECT COUNT(*) AS openJobs FROM CURRENT_JOB;`;
+    
+    const result = await executeQuery(sql);
+    
+    // if there is a result return the openJobs count or need to return 0 to prevent error
+    if (result[0]) {
+      return result[0].openJobs;
+    } else {
+      return 0;
+    }
+  };
   
-    // need to delete associated workers first then the business
-    await executeQuery(sqlWorkers, [businessId]);
-    return executeQuery(sqlBusiness, [businessId]);
+  // Get the total number of jobs created
+  export const countTotalJobs = async () => {
+    // SQL query to count the total number of jobs
+    const sql = `SELECT COUNT(*) AS totalJobs FROM JOB_TABLE;`;
+    
+    const result = await executeQuery(sql);
+    
+    // if there is a result return the totalJobs count or need to return 0 to prevent error
+    if (result[0]) {
+      return result[0].totalJobs;
+    } else {
+      return 0;
+    }
   };
 
-  
+// Search for employees by name or ID
+export const searchEmployees = async (searchTerm) => {
+  const sql = `
+    SELECT * FROM WORKER_TABLE
+    WHERE Username LIKE ? OR Worker_ID = ?;
+  `;
+  const term = `%${searchTerm}%`;
+  return executeQuery(sql, [term, searchTerm]);
+};
+
+// Change privilege levels
+export const changePrivilegeLevel = async (workerId, newPrivilegeLevel) => {
+  const sql = `
+    UPDATE WORKER_TABLE
+    SET Privilege_level = ?
+    WHERE Worker_ID = ?;
+  `;
+  return executeQuery(sql, [newPrivilegeLevel, workerId]);
+};
+
+
+
