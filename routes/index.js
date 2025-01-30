2d6341b08c561e279b5f94a97e45b973573dd5ac
Vuk Stojkovic <vuk.stojkovic@student.manchester.ac.uk>
2025-01-30 14:24:25 +0000

    Endpoints for current_job and job_history have been exposed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,51 @@
 import express from 'express';
+import { fetchAll,createConnection,execute } from '../dbhelper.js';
 var indexRouter = express.Router();
+
 
 /* GET home page. */
 indexRouter.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+indexRouter.get('/current_jobs/:bid', async (req,res)=>{
+  // get data from db and send as json to front-end
+  // bid = business id
+  const businessId = req.params.bid
+  //construct sql query
+  const sql = 'SELECT * FROM CURRENT_JOB WHERE User_id = ?'
+
+  try{
+    const connection = await createConnection()
+    const jobs = fetchAll(connection,sql,[businessId])
+    res.json(jobs)
+  }catch(err){  
+    res.json({error:err})
+  }
+
+})
+
+indexRouter.get('/job_history/:bid', async (req,res)=>{
+  // get data from db and send as json to front-end
+  // bid = business id
+  const businessId = req.params.bid
+  //construct sql query
+  const sql = 'SELECT * FROM JOB_HISTORY WHERE User_id = ?'
+
+  try{
+    const connection = await createConnection()
+    const jobs = fetchAll(connection,sql,[businessId])
+    res.json(jobs)
+  }catch(err){  
+    res.json({error:err})
+  }
+})
+
+indexRouter.post('/send_nottification/:jid', async (req,res)=>{
+  // get the customer endpoints from the db and send a nottification through web-push
+  const jobId = req.params.jid
+
+  
+})
 export { indexRouter };
