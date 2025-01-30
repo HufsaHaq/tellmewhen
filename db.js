f9b1c083bb9dff12f0ccb569dc63947a5ff02695
Hufsa Haq <hufsahaq@gmail.com>
2025-01-30 14:22:22 +0000

    gen hleper + subscription edit

--- db.js
+++ db.js
@@ -97,6 +97,7 @@ db.connect((err) => {
         Auth_Key1 TEXT NOT NULL,
         Auth_Key2 TEXT NOT NULL,
         Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+        FOREIGN KEY (Job_ID) REFERENCES JOB_TABLE(Job_ID)
     )
     `,
   ];
