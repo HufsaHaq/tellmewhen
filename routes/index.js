4db130f8d656685fcaa4095eb96f3fe4ce28f6c5
Vuk Stojkovic <vuk.stojkovic@student.manchester.ac.uk>
2025-01-31 15:43:13 +0000

    History query refactored

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,42 @@
 import express from 'express';
-import { fetchAll,createConnection,execute } from '../dbhelper.js';
+import { fetchAll, createConnection } from '../dbhelper.js';
+
 var indexRouter = express.Router();
 
-
 /* GET home page. */
-indexRouter.get('/', function(req, res, next) {
+indexRouter.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+indexRouter.get('/current_jobs/:bid', async (req, res) => {
+  const businessId = req.params.bid;
+  const sql = 'SELECT * FROM CURRENT_JOB WHERE User_id = ?';
 
-indexRouter.get('/current_jobs/:bid', async (req,res)=>{
-  // get data from db and send as json to front-end
-  // bid = business id
-  const businessId = req.params.bid
-  //construct sql query
-  const sql = 'SELECT * FROM CURRENT_JOB WHERE User_id = ?'
-
-  try{
-    const connection = await createConnection()
-    const jobs = fetchAll(connection,sql,[businessId])
-    res.json(jobs)
-  }catch(err){  
-    res.json({error:err})
+  try {
+    const connection = await createConnection();
+    const jobs = await fetchAll(connection, sql, [businessId]);
+    res.json(jobs);
+    connection.end(); // Close the connection
+  } catch (err) {
+    res.json({ error: err });
   }
+});
 
-})
+indexRouter.get('/job_history/:bid', async (req, res) => {
+  const businessId = req.params.bid;
+  const sql = 'SELECT * FROM JOB_HISTORY WHERE User_id = ?';
 
-indexRouter.get('/job_history/:bid', async (req,res)=>{
-  // get data from db and send as json to front-end
-  // bid = business id
-  const businessId = req.params.bid
-  //construct sql query
-  const sql = 'SELECT * FROM JOB_HISTORY WHERE User_id = ?'
-
-  try{
-    const connection = await createConnection()
-    const jobs = fetchAll(connection,sql,[businessId])
-    res.json(jobs)
-  }catch(err){ 
-    res.json({error:err})
+  try {
+    const connection = await createConnection();
+    const jobs = await fetchAll(connection, sql, [businessId]);
+    res.json(jobs);
+    connection.end(); // Close the connection
+  } catch (err) {
+    res.json({ error: err });
   }
-})
-export { indexRouter };
+});
+
+indexRouter.post('/job:id/notify')
+
+
+export { indexRouter }; 
