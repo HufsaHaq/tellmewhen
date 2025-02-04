e1028b9b4bb895be92a8f5d35af28ed18ca911ef
Archie <archie.calvert@outlook.com>
2025-02-04 11:32:37 +0000

    Added in skeleton API calls

---
+++ tellmewhen/src/API/account.js
@@ -0,0 +1,55 @@
+function AddWorker(username, password, businessID, privilegeLevel, accessToken)
+{
+    let base = localStorage["endpoint"];
+}
+
+function DeleteWorker(workeridTARGET, workeridSENDER)
+{
+
+}
+
+function EditUserLogin(workerID, username, password, accessToken)
+{
+
+}
+
+function DeleteBusiness(businessID, accessToken)
+{
+
+}
+
+function CountOpenJobs(businessID, accessToken)
+{
+
+}
+
+function CountTotalJobs(businessID, accessToken)
+{
+
+}
+
+function SearchEmployees(searchTerm, accessToken)
+{
+
+}
+
+
+function ChangePrivilege(workerID, newLevel, accessToken)
+{
+
+}
+
+function GetBusinessDetails(businessID, accessToken)
+{
+
+}
+
+function RenameBusiness(businessID, newName, accessToken)
+{
+
+}
+
+function ChangeBusinessProfilePhoto(businessID, base64img, accessToken)
+{
+
+}
---
+++ tellmewhen/src/API/dashboard.js
@@ -0,0 +1,24 @@
+function GetActiveJobs(userID, accessToken)
+{
+
+}
+
+function GetJobHistory(userID, accessToken)
+{
+
+}
+
+function AddJob(userID, jobID, description, url, dueDate, accessToken)
+{
+
+}
+
+function AssignJobToWorker(userID, jobID, accessToken)
+{
+
+}
+
+function CompleteJob(userID, jobID, remarks, accessToken)
+{
+
+}
