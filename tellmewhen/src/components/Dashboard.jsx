a472f7af05310933f3c69fcb3b1e357e3b33870e
Palak Singh <palak.singh@student.manchester.ac.uk>
2025-02-07 15:49:34 +0000

    Increased text size

--- tellmewhen/src/components/Dashboard.jsx
+++ tellmewhen/src/components/Dashboard.jsx
@@ -9,7 +9,7 @@ const DashboardButton = () => {
 	return (
 		<button
 		  onClick={handleDashboardButton}
-		  className = "px-6 py-3 bg-blue-500 text-white rounded-full hover:bg-blue-700"
+		  className = "px-6 py-3 text-2xl bg-blue-500 text-white rounded-full hover:bg-blue-700"
 		>
 		  Back to Dashboard
 		</button>
