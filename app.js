ab34ae8af2682c924c4bdfb3ab9547b84b628cef
Vuk Stojkovic <vuk.stojkovic@student.manchester.ac.uk>
2025-02-21 11:19:57 +0000

    Resolved router issue

--- app.js
+++ app.js
@@ -34,13 +34,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/qr', qrRouter);
 app.use('/delete', deletionRouter)
-<<<<<<< Updated upstream
+
 app.use('/jobs', jobRouter)
 app.use("/business", businessRouter);
-=======
+
 app.use('/jobs',jobRouter)
 app.use('/customer',customerRouter)
->>>>>>> Stashed changes
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
