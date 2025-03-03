c689eff54b4b25b8cc1261c054905ba3708da51c
Vuk Stojkovic <vuk.stojkovic@student.manchester.ac.uk>
2025-03-03 11:27:28 +0000

    Function name changes

--- db.js
+++ db.js
@@ -199,7 +199,6 @@ export const populateDatabase = async () => {
   await createjobhistory();
   await createsubscription_table();
   await createnotification();
-  await createaccesstoken();  
   //await populateDatabase(); 
 })();
 
