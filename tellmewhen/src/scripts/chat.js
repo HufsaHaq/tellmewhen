aee9fb3acaf2504efe6d07cd14b146bc03f23a88
Hufsa Haq <hufsahaq@gmail.com>
2025-03-06 20:03:51 +0000

    frontend endpoints new

--- tellmewhen/src/scripts/chat.js
+++ tellmewhen/src/scripts/chat.js
@@ -1,3 +1,4 @@
+/* archie stuff 
 import axois from "axios";
 export async function NewUser(username, id) {
     let base = localStorage["endpointChat"];
@@ -47,4 +48,76 @@ export async function NewChannel(name, id)
             window.location.href = "/debugger";
         }
     })
+}
+*/
+import axios from "axios";
+
+export async function NewUser(jobId) {
+    let base = localStorage["endpointChat"];
+    if (localStorage["endpointChat"] == null || localStorage["endpointChat"] == "") {
+        throw new Error("Endpoint not defined.");
+    }
+
+    await axios.post(base + "/chat/create_user", {
+        jobId: jobId,
+    }).then(res => {
+        console.log(res.status);
+        if (res.status === 200) {
+            console.log("Guest user created successfully");
+            return res.data.token; // return the guest token
+        }
+    });
+}
+
+export async function LogIn(userId, businessId) {
+    let base = localStorage["endpointChat"];
+    let data;
+    if (localStorage["endpointChat"] == null || localStorage["endpointChat"] == "") {
+        throw new Error("Endpoint not defined.");
+    }
+
+    await axios.get(base + "/chat/worker/login/" + userId + "/" + businessId)
+        .then(res => {
+            console.log(res.status);
+            if (res.status === 200) {
+                console.log("Worker logged in successfully");
+                data = res.data; // rturn the token and channels
+            }
+        });
+
+    return data;
+}
+
+export async function NewChannel(jobId, businessId) {
+    let base = localStorage["endpointChat"];
+    if (localStorage["endpointChat"] == null || localStorage["endpointChat"] == "") {
+        throw new Error("Endpoint not defined.");
+    }
+
+    await axios.post(base + "/chat/channels/create_channel/" + businessId, {
+        jobId: jobId,
+    }).then(res => {
+        console.log(res.status);
+        if (res.status === 200) {
+            console.log("Channel created successfully");
+            return res.data.channel; // return the created channel
+        }
+    });
+}
+
+export async function DeleteChannel(jobId) {
+    let base = localStorage["endpointChat"];
+    if (localStorage["endpointChat"] == null || localStorage["endpointChat"] == "") {
+        throw new Error("Endpoint not defined.");
+    }
+
+    await axios.delete(base + "/chat/channels/delete_channel", {
+        data: { jobId: jobId },
+    }).then(res => {
+        console.log(res.status);
+        if (res.status === 200) {
+            console.log("Channel deleted successfully");
+            return res.data; // return the deletion result
+        }
+    });
 }
