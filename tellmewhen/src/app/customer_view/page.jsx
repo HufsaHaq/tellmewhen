23e21a39958821956bcbda398d5f5d8926fa7cd5
Muhammed Choudhary <muhammed@Muhammeds-MacBook-Air.local>
2025-03-06 13:08:47 +0000

    added 2 functions

--- tellmewhen/src/app/customer_view/page.jsx
+++ tellmewhen/src/app/customer_view/page.jsx
@@ -13,6 +13,31 @@ function Page() {
     checkSubscriptionStatus();
   }, []);
 
+  function urlB64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/-/g, '+')
+        .replace(/_/g, '/');
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; i++) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+  }
+
+  async function saveSubscription(subscription) {
+    let endpoint = localStorage["endpoint"];
+    const response = await fetch(endpoint + '/save-new-subscription', {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            ...subscription,
+            jobId: window.location.pathname.split('/').pop()
+        })
+    });
+}
   const checkSubscriptionStatus = async () => {
     if ("serviceWorker" in navigator) {
       const registration = await navigator.serviceWorker.ready;
