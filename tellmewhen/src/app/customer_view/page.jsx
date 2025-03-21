a174b05024309483501882b1c3f30805f1a44c29
Hufsa Haq <hufsahaq@gmail.com>
2025-03-21 10:13:36 +0000

    update pwa 2

--- tellmewhen/src/app/customer_view/page.jsx
+++ tellmewhen/src/app/customer_view/page.jsx
@@ -4,8 +4,10 @@ import PageLoad from "@/components/PageLoad";
 import { useEffect } from "react";
 export default function Page()
 {
+    useEffect(() => {
 
             window.location.href = "/customer_view/" + localStorage["encryptedJobID"]
-
+        
+    }, [])
     return <PageLoad></PageLoad>
 }
